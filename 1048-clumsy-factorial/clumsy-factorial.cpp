de330c04f12316a366724a67e56878cb5e17d3ea
rudra2311-patel <113626665+rudra2311-patel@users.noreply.github.com>
2025-08-29 12:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.42%) - LeetSync

--- 1048-clumsy-factorial/clumsy-factorial.cpp
+++ 1048-clumsy-factorial/clumsy-factorial.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int clumsy(int n) {
-        int sum = 0;
-
-        if (n == 1) return 1;
-        if (n == 2) return 2 * 1;
-        if (n == 3) return (3 * 2) / 1;
-
-        for (int i = n; i > 0; i -= 4) {
-            if (i == n) {
-                sum += (i * (i - 1)) / (i - 2) + (i - 3 > 0 ? i - 3 : 0);
-            } else {
-                if (i >= 3) {
-                    sum -= (i * (i - 1)) / (i - 2);
-                    if (i - 3 > 0) sum += (i - 3);
-                } else if (i == 2) {
-                    sum -= 2 * 1;
-                } else if (i == 1) {
-                    sum -= 1;
-                }
-            }
+    int clumsy(int N) {
+        if (N <= 2) return N;
+        if (N <= 4) return N + 3;
+        
+        if ((N - 4) % 4 == 0) {
+            return N + 1;
+        } else if ((N - 4) % 4 <= 2) {
+            return N + 2;
+        } else {
+            return N - 1;
         }
-
-        return sum;
     }
 };

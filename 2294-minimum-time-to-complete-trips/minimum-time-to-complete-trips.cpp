e6dc39730dbd64ff6651993cff0956253aded90c
Patel Rudra <113626665+rudra2311-patel@users.noreply.github.com>
2025-09-22 01:35:13 +0530

    Time: 91 ms (11.17%) | Memory: 98.4 MB (50.98%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     long long minimumTime(vector<int>& time, int totalTrips) {
         long long left = 1;
-        long long right = 1e14;
+        long long right = 1e14; 
         long long ans = right;
 
         while(left <= right) {
             long long mid = left + (right - left) / 2;
-            long long trips = 0;
+            long long sum = 0;
             for(int i = 0; i < time.size(); i++) {
-                trips += mid / time[i];
+                sum += mid / time[i];
             }
 
-            if(trips >= totalTrips) {
-                ans = mid;      
-                right = mid - 1; 
+            if(sum < totalTrips) {
+                left = mid + 1;
             } else {
-                left = mid + 1; 
+                ans = mid;      
+                right = mid - 1;
             }
         }
         return ans;

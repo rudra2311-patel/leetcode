a66ab6ce6aad5e59c7331c3aef2dc2ac8d36e33c
Patel Rudra <113626665+rudra2311-patel@users.noreply.github.com>
2025-09-21 23:07:13 +0530

    Time: 345 ms (70.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+SELECT 
+    query_name, 
+    ROUND(SUM(q.rating / q.position) / COUNT(rating), 2) AS quality,
+    ROUND(
+        (SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0) / COUNT(rating),
+        2
+    ) AS poor_query_percentage
+FROM Queries q
+GROUP BY query_name;

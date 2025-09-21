d05eb645d38d17e6390c5551c2a0c1f3cc3c3f50
Patel Rudra <113626665+rudra2311-patel@users.noreply.github.com>
2025-09-22 01:36:14 +0530

    Time: 115 ms (5.03%) | Memory: 98.3 MB (85.34%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -3,7 +3,7 @@ public:
     long long minimumTime(vector<int>& time, int totalTrips) {
         long long left = 1;
         long long right = 1e14; 
-        long long ans = right;
+
 
         while(left <= right) {
             long long mid = left + (right - left) / 2;
@@ -15,10 +15,10 @@ public:
             if(sum < totalTrips) {
                 left = mid + 1;
             } else {
-                ans = mid;      
+                   
                 right = mid - 1;
             }
         }
-        return ans;
+        return left;
     }
 };

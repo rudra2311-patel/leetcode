a56ce5124984c297cfc0eb4d42a1214051eb8243
Patel Rudra <113626665+rudra2311-patel@users.noreply.github.com>
2025-09-23 21:11:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (9.51%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        string s = to_string(num);
-        int len = s.length();
-        int sum = 0;
-        if(len ==1) return num;
-        while (len > 1) {
-            sum = 0;
-            for (int i = 0; i < len; i++) {
-                sum += s[i] - '0';
-            }
-            s = to_string(sum);
-            len = s.length();
-        }
-
-        return sum;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 };

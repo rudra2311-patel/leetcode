3c5cc2fd81164dbaa98cd54f20bb76f0d49b6a75
Patel Rudra <113626665+rudra2311-patel@users.noreply.github.com>
2025-10-25 23:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.15%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -4,19 +4,13 @@ public:
         int div = n/7;
         int rem = n % 7;
         int ans = 0;
-        if(div!=0){
                 int new1 = (((div+rem) - (div+1) + 1) * ((div+rem) + (div +1)))/2;
                 for(int i=0;i<div;i++){
                     ans = ans + 28 + (7*i);
                   
                 }
                   ans = ans + new1;
-                
-        }
-        else{
-            int newi = (n * (n+1))/2;
-            ans = ans + newi;
-        }
+
     return ans;
-    }
+    }  
 };

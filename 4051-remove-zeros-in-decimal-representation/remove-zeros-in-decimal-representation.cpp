9cdea2e9225a94d504c1e03c5d42e27f1c741d32
Patel Rudra <113626665+rudra2311-patel@users.noreply.github.com>
2025-10-26 10:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (100.00%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     long long removeZeros(long long n) {
-        vector<int> rudra;  
-        int rem;
-
-        
-        while (n > 0) {
-            rem = n % 10;
-            if (rem != 0) {
-                rudra.push_back(rem);
-            }
-            n = n / 10;
-        }
-
         long long ans = 0;
-        for (int i = rudra.size() - 1; i >= 0; i--) {
-            ans = ans * 10 + rudra[i];
+        long long place = 1;  
+
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem != 0) {
+                ans += rem * place;  
+                place *= 10;
+            }
+            n /= 10;
         }
 
         return ans;
